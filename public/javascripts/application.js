c3652a7cf9d039325c51b4a59e647ae7cea4b7a2
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-02-07 12:40:16 -0600

    Loading underscore via require.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,11 +3,12 @@ requirejs.config({
 
   paths: {
     'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
+    'underscore': 'underscore/underscore-min'
   },
 
 });
 
 
-require(["jquery"], function($) {
+require(["jquery", "underscore"], function($, _) {
 });
 
