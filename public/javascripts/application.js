0675d9ff56633be73cf3691b7edac2b5485b96c6
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-02-07 12:34:50 -0600

    Moved bootstrap around

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,13 @@
 requirejs.config({
   baseUrl: 'javascripts/lib',
+
   paths: {
-    'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min' 
-  }
+    'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
+  },
+
 });
 
 
 require(["jquery"], function($) {
-  $(function() {
-  });
 });
 
