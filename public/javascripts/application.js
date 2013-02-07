2a48d8f7f75f6e19dab82505cdaf6df7d5c4cb96
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2013-02-07 13:04:03 -0600

    Got backbone all loaded

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,12 +3,19 @@ requirejs.config({
 
   paths: {
     'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min',
-    'underscore': 'underscore/underscore-min'
+    'underscore': 'underscore/underscore-min',
+    'backbone': 'backbone/backbone-min'
   },
+  shim: {
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    }
+  }
 
 });
 
 
-require(["jquery", "underscore"], function($, _) {
+require(["jquery", "underscore", "backbone"], function($, _, Backbone) {
 });
 
